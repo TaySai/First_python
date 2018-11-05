09fe8f83c2e0c558b206a08c4f3dc3797e627432
TaySai <saidi.tayeb@hotmail.com>
2018-11-05 14:45:39 +0100

    add exercise

--- src/exercise_6_nov.py
+++ src/exercise_6_nov.py
@@ -1,27 +1,31 @@
 # Validate all strings with numbers and only one "." (point)
-
 sentence = input('Type your text: ')
 tester = None
 message = "Good! Your text respect the conditions"
 for letter in sentence:
     # testing if the letter is a point
-    if letter == ".":
+    if letter == "." or letter == ",":
         # testing if it's the first point
         if not tester or tester is None:
             tester = True
         else:
             # if not first point
             tester = False
-            message = 'you have more than one "."'
+            message = 'you have more than one "." or ","'
             break
     # testing if the letter is a decimal or not
-    elif not letter.isdecimal():
-        message = 'you must have only numbers and one and only one "."'
-        tester = False
-        break
+    else:
+        try:
+            # try to convert text to int
+            x = int(letter)
+        except ValueError:
+            # return error if in contains alphabetic chars
+            message = 'Alphabetic char detected, you must have only numbers and one and only one "." or ","'
+            tester = False
+            break
 # display text depending on the result
 if tester is None:
-    print('you forgot to add a "."')
+    print('this is an integer and not a float')
 elif tester:
     print(message)
 else:
--- src/exercises_for_6_nov/question1.py
+++
@@ -1,9 +0,0 @@
-# Reading a value on the command line and converting it into a float
-try:
-    # try to convert int to float
-    x = int(input("Please enter a number: "))
-    x = float(x)
-    print(x)
-except ValueError:
-    # return error if in contains alphabetic chars
-    print("Oops! this is not a number")
